a9b193c8752cd4f0df6449043c7df90b3d327b62
Luis Frontanilla <lrdolucho@gmail.com>
2020-12-22 02:04:49 -0400

    Improved Cell and added CellContent

---
+++ core/src/main/java/core/Screens/game/stuff/CellContent.java
@@ -0,0 +1,24 @@
+package core.Screens.game.stuff;
+
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public abstract class CellContent {
+
+    protected final Sprite sprite;
+    protected final Cell cell;
+
+    public CellContent(Sprite cellContentSprite, Cell cell) {
+        sprite = new Sprite(cellContentSprite);
+        sprite.setPosition(cell.getX(), cell.getY());
+        this.cell = cell;
+    }
+
+    public void draw(SpriteBatch spriteBatch) {
+        sprite.draw(spriteBatch);
+    }
+
+    public Cell getCell() {
+        return cell;
+    }
+}
